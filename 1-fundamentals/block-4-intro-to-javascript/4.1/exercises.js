0e10f1cabf34119ee77d24e54d8064410bc00a16
rCirelli <raphael.cirelli@protonmail.com>
2022-03-08 18:47:20 -0300

    altera nomes das pastas

---
+++ 1-fundamentals/block-4-intro-to-javascript/4.1/exercises.js
@@ -0,0 +1,82 @@
+// const myName = "Raphael";
+// const birthCity = "Cafelandia";
+// let birthYear = "1994";
+// 
+// console.log(myName, birthCity, birthYear);
+
+// let patientId = 50;
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: 'Ana',
+//   lastName: 'Santos',
+// };
+// const patientEmail = 'ana@email.com';
+
+// console.log(typeof patientId);
+// console.log(typeof isEnrolled);
+// console.log(typeof patientInfo);
+// console.log(typeof patientEmail);
+// console.log(typeof patientAge);
+
+//Exercício calculo de área e perimetro de um retangulo:
+// const base = 5;
+// const height = 8;
+// const area = base*height;
+// const perimeter = (base*2)+(height*2);
+
+// console.log(area);
+// console.log(perimeter);
+
+//Exercício verifica se candidato foi aprovado baseado em sua nota
+// const grade = 42;
+
+// if (grade >= 80) {
+//     console.log("Parabéns, você foi aprovada(o)!");
+// }
+// else if (grade < 80 && grade >= 60) {
+//     console.log("Você está na nossa lista de espera");
+// }
+// else {
+//     console.log("Você foi reprovada(o)");
+// }
+
+// const conditionOne = true;
+// const conditionTwo = false;
+
+// console.log(conditionOne && conditionTwo);
+
+// const currentHour = 4;
+// let message = "";
+
+// if(currentHour >= 22){
+//     message = "Não deveriamos comer nada, é hora de dormir";
+// }else if(currentHour >= 18 && currentHour < 22){
+//     message = "Rango da noite, vamos jantar :D";
+// }else if (currentHour >=14 && currentHour < 18){
+//     message = "Vamos fazer um bolo pro café da tarde?";
+// }else if(currentHour >= 11 && currentHour <= 14){
+//     message = "Hora do almoço!!!";
+// }else if(currentHour >= 4 && currentHour <= 11) {
+//     message = "Hmm, cheiro de café recem passado";
+// }
+// console.log(message);
+
+// let state = "nada";
+
+// switch (state) {
+//     case "aprovada":
+//         console.log("aprovada");
+//         break;
+    
+//     case "lista":
+//         console.log("lista");
+//         break;
+    
+//     case "reprovada":
+//         console.log("reprovada");
+//         break
+
+//     default:
+//         console.log("não se aplica")  
+// }
+
