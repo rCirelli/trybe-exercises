3c4be675ad48a6ab84d69e90d6d5212b34d849ca
rCirelli <raphael.cirelli@protonmail.com>
2022-03-14 18:06:19 -0300

    feitos os exercicios de fixação

---
+++ 1-fundamentals/block-5-javascript-dom-events-and-web-storage/5.1/exercises/time-manager/script.js
@@ -0,0 +1,16 @@
+/*header*/
+document.getElementById('header-container').style.color  = 'white';
+document.getElementById('header-container').style.backgroundColor  = '#02b26e';
+document.querySelector('#header-container h1').style.backgroundColor  = '#02b26e';
+
+/*caixas urgentes*/
+document.querySelectorAll('.emergency-tasks div')[0].style.backgroundColor = '#ff9f86';
+document.querySelectorAll('.emergency-tasks div')[1].style.backgroundColor = '#ff9f86';
+document.querySelectorAll('.emergency-tasks h3')[0].style.backgroundColor = '#a500ee';
+document.querySelectorAll('.emergency-tasks h3')[1].style.backgroundColor = '#a500ee';
+
+/*caixas não urgentes*/
+document.querySelectorAll('.no-emergency-tasks div')[0].style.backgroundColor = '#f9dd69';
+document.querySelectorAll('.no-emergency-tasks div')[1].style.backgroundColor = '#f9dd69';
+document.querySelectorAll('.no-emergency-tasks h3')[0].style.backgroundColor = '#232525';
+document.querySelectorAll('.no-emergency-tasks h3')[1].style.backgroundColor = '#232525';
