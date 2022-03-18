5ec19a8b6941829f237dfd836461e8a4e9bf3ac2
rCirelli <raphael.cirelli@protonmail.com>
2022-03-18 18:29:28 -0300

    conclui alguns exercícios

--- 1-fundamentals/block-4-intro-to-javascript/4.4/challenges/2-functions.js
+++ 1-fundamentals/block-4-intro-to-javascript/4.4/challenges/2-functions.js
@@ -15,7 +15,7 @@
 //2 - Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
 
 // function checkBiggestNum(...args) {
-//   //na linha de código abaixo e na definição dos parametros da função, usei informção retirada da seguinte documentação: https://github.com/airbnb/javascript#es6-rest
+//na linha de código abaixo e na definição dos parametros da função, usei informção retirada da seguinte documentação: https://github.com/airbnb/javascript#es6-rest
 //   const numbers = args;
 //   let checkArray = true;
 //   let biggestNum = 0;
@@ -37,7 +37,7 @@
 //3 - Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
 
 // function checkSmallNum(...args) {
-//   //na linha de código abaixo e na definição dos parametros da função, usei informção retirada da seguinte documentação: https://github.com/airbnb/javascript#es6-rest
+//na linha de código abaixo e na definição dos parametros da função, usei informção retirada da seguinte documentação: https://github.com/airbnb/javascript#es6-rest
 //   const numbers = args;
 //   let checkArray = true;
 //   let smallestNum = numbers[0];
@@ -59,7 +59,7 @@
 //4 - Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
 
 // function checkBigWord(...args) {
-  //na linha de código abaixo e na definição dos parametros da função, usei informção retirada da seguinte documentação: https://github.com/airbnb/javascript#es6-rest
+//na linha de código abaixo e na definição dos parametros da função, usei informção retirada da seguinte documentação: https://github.com/airbnb/javascript#es6-rest
 //   const words = args;
 //   let checkArray = true;
 //   let smallestWord = words[0];
@@ -80,24 +80,20 @@
 
 //5 - Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
 
-function checkRepeatingNum(...args) {
-  //na linha de código abaixo e na definição dos parametros da função, usei informção retirada da seguinte documentação: https://github.com/airbnb/javascript#es6-rest
-  const numbers = args;
-  let checkArray = true;
-  let mostRepeats = 0;
+// function checkRepeatingNum(args = []) {
+// }
 
-  for (let i in numbers) {
-    if (typeof numbers[i] !== "number") {
-      checkArray = false;
-      console.log('Insira apenas números inteiros como parametros');
-    } else if (numbers[i] > mostRepeats) {
-        mostRepeats = numbers[i];
-    }
-  }
-  
-  return numbers.indexOf(mostRepeats);
-}
 
-console.log(checkRepeatingNum(125.99, 2, 50, 20, 40, 700, 701));
+// 6 - Crie uma função que receba um número natural (número inteiro não negativo) N e retorne o somatório de todos os números de 1 até N.
 
+// function sumToN(n = NaN) {
+//   let sum = 0;
+
+//   for (let i = 0; i <= n; i += 1){
+//     sum += i;
+//   }
+//   return sum;
+// }
+
+// console.log(sumToN(6));
 
