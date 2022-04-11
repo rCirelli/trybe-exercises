ff57c6e88bcdef30951dc45c9575f2b6ee2f2c0f
rCirelli <raphael.cirelli@protonmail.com>
2022-04-11 16:00:22 -0300

    inicia exercícios do dia

---
+++ 1-fundamentals/block-8-JS-ES6-higher-order-functions/8.3/exercises.js
@@ -0,0 +1,24 @@
+const products = ['Arroz', 'Feijão', 'Alface', 'Tomate'];
+const prices = [2.99, 3.99, 1.5, 2];
+
+const updateProducts = (productsList, pricesList) => (
+  productsList.map((product, index) => ({ [product]: pricesList[index] }))
+);
+
+const pricedProducts = updateProducts(products, prices);
+
+// console.log(pricedProducts);
+
+const numeros = [1, 2, 3, 4, 5, 6];
+console.log(numeros.map((numero) => numero * 2)); // Retorno: [2, 4, 6, 8, 10, 12]
+
+const paresMenoresQueCinco = [];
+numeros.forEach((numero) => {
+  if (numero < 5 && numero % 2 === 0) {
+    paresMenoresQueCinco.push(numero);
+  }
+});
+console.log(paresMenoresQueCinco); // Retorno: [2, 4]
+
+const paresMenoresQueCincoFilter = numeros.filter((numero) => numero < 5 && numero % 2 ===0);
+console.log(paresMenoresQueCincoMap);
