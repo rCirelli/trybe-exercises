fc9081f841c5746ea92e83a8f9689183590289a8
rCirelli <raphael.cirelli@protonmail.com>
2022-04-29 15:37:21 -0300

    exercícios concluidos

--- 1-fundamentals/block-8-JS-ES6-higher-order-functions/8.1/exercise3.js
+++ 1-fundamentals/block-8-JS-ES6-higher-order-functions/8.1/exercise3.js
@@ -3,16 +3,14 @@ const checkAnswers = (rightAnswers, studentAnswers) => {
   for (let i = 0; i < studentAnswers.length; i += 1) {
     if (studentAnswers[i] !== 'N.A') {
       studentAnswers[i] === rightAnswers[i] ? count += 1 : count -= 0.5;
-      console.log(`${parseInt(i) + 1}º if: ${count}`);
     }
-    else console.log(`${parseInt(i) + 1}º if: N.A`);
   }
   return count;
 };
 
-const giveGrade = (rightAnswers, studentAnswers, callback) => `Acertou ${callback(rightAnswers, studentAnswers)} respostas.`;
+const giveGrade = (rightAnswers, studentAnswers, callback) => `A nota é ${callback(rightAnswers, studentAnswers)}.`;
 
-const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const RIGHT_ANSWERS =   ['A',  'C',  'B', 'D', 'A', 'A',  'D',  'A', 'D', 'C'];
 const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
 
 console.log(giveGrade(RIGHT_ANSWERS, STUDENT_ANSWERS, checkAnswers));
