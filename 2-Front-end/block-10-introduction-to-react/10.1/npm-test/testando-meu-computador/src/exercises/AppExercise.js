04814452dd9626c403cab5fe0046567c5a7a700a
rCirelli <raphael.cirelli@protonmail.com>
2022-05-05 21:10:20 -0300

    exercícios do dia concluidos

--- 2-Front-end/block-10-introduction-to-react/10.1/npm-test/testando-meu-computador/src/exercises/AppExercise.js
+++ 2-Front-end/block-10-introduction-to-react/10.1/npm-test/testando-meu-computador/src/exercises/AppExercise.js
@@ -1,10 +1,33 @@
 import MyComponent from './Component';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
+import './exercises.css';
+
+const listArray = ['first item', 'second item', 'third item', 'fourth item ', 'fifth item'];
+
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+  
+const getList = listArray.map((item) => Task(item));
 
 function AppExercise() {
   return (
     <div>
-      <p>Hello World!</p>
-      <MyComponent />
+      <Header />
+      {/* <p>Hello World!</p> */}
+      {/* <MyComponent /> */}
+      <div className='content heading'>
+        <h2>Exercícios do dia 10.1</h2>
+        <ol>
+          {getList}
+        </ol>
+      </div>
+      <Content />
+      <Footer />
     </div>
   );
 }
