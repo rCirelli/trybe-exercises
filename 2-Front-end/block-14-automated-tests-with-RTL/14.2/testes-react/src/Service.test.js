0fa8436c419b258510eb4f8288a3308a0770b238
Raphael Cirelli <raphael.cirelli@protonmail.com>
2022-06-02 19:45:56 -0300

    exercise 2

--- 2-Front-end/block-14-automated-tests-with-RTL/14.2/testes-react/src/Service.test.js
+++ 2-Front-end/block-14-automated-tests-with-RTL/14.2/testes-react/src/Service.test.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import App from './App';
+// import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
 
 test('Exercício 1: teste da função getRandomNumber', () => {
-  render(<App />);
   const service = require('./logic/service');
-  service.getRandomNumber = jest.fn(() => 10);
+  // service.getRandomNumber = jest.fn(() => 10);
+  jest.mock('./logic/service');
+  service.getRandomNumber.mockImplementation(() => 10);
 
   const result = service.getRandomNumber();
   expect(service.getRandomNumber).toHaveBeenCalledTimes(1);
   expect(result).toBe(10);
+});
+
+test('Exercício 2: altera função getRandomNumber', () => {
+  const service = require('./logic/service');
+  jest.mock('./logic/service');
+  service.getRandomNumber.mockImplementationOnce((a, b) => a / b);
+
+  const result = service.getRandomNumber(10, 2);
+  expect(service.getRandomNumber).toHaveBeenCalledTimes(1);
+  expect(result).toBe(5);
 });
