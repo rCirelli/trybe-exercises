82086491d89c374be54c757939148326bb0320e9
Raphael Cirelli <raphael.cirelli@protonmail.com>
2022-06-02 20:45:38 -0300

    exercise 4 finished

--- 2-Front-end/block-14-automated-tests-with-RTL/14.2/testes-react/src/Service.test.js
+++ 2-Front-end/block-14-automated-tests-with-RTL/14.2/testes-react/src/Service.test.js
@@ -2,6 +2,8 @@
 // import { render, screen } from '@testing-library/react';
 // import userEvent from '@testing-library/user-event';
 
+afterEach(() => jest.clearAllMocks());
+
 test('Exercício 1: teste da função getRandomNumber', () => {
   const service = require('./logic/service');
   // service.getRandomNumber = jest.fn(() => 10);
@@ -20,6 +22,7 @@ test('Exercício 2: altera função getRandomNumber', () => {
 
   const result = service.getRandomNumber(10, 2);
   expect(service.getRandomNumber).toHaveBeenCalledTimes(1);
+  expect(service.getRandomNumber).toHaveBeenCalledWith(10, 2);
   expect(result).toBe(5);
 });
 
@@ -37,6 +40,42 @@ test('Exercício 3', () => {
   mockMultiply.mockImplementation((a) => a*2);
   const result2 = mockMultiply(100);
   expect(mockMultiply).toHaveBeenCalledTimes(1);
+  expect(mockMultiply).toHaveBeenCalledWith(100);
   expect(result2).toBe(200);
 
+  mockMultiply.mockRestore();
+});
+
+test('Exercício 4', () => {
+  const service = require('./logic/service');
+  const mockUpperCase = jest.spyOn(service, 'upperCase')
+    .mockImplementation((string) => string.toLowerCase());
+
+  const result = mockUpperCase('TEST');
+  expect(result).toBe('test');
+  expect(service.upperCase).toBeCalledWith('TEST');
+  expect(service.upperCase).toHaveBeenCalledTimes(1);
+
+  const mockFirstLetter = jest.spyOn(service, 'firstLetter')
+  .mockImplementation((string) => string[string.length - 1]);
+
+  const result2 = mockFirstLetter('test');
+  expect(result2).toBe('t');
+  expect(service.firstLetter).toBeCalledWith('test');
+  expect(service.firstLetter).toHaveBeenCalledTimes(1);
+
+  const mockConcatStr = jest.spyOn(service, 'concatStr')
+  .mockImplementation((str1, str2, str3) => `${str1}${str2}${str3}`);
+
+  const result3 = mockConcatStr('ab', 'cd', 'ef');
+  expect(result3).toBe('abcdef');
+  expect(service.concatStr).toBeCalledWith('ab', 'cd', 'ef');
+  expect(service.concatStr).toHaveBeenCalledTimes(1);
+
+  service.upperCase.mockRestore();
+
+  const result4 = service.upperCase('test');
+  expect(result4).toBe('TEST');
+  expect(service.upperCase).toBeCalledWith('test');
+  expect(service.upperCase).toHaveBeenCalledTimes(1);
 });
--- 2-Front-end/block-14-automated-tests-with-RTL/14.2/testes-react/src/logic/service.js
+++ 2-Front-end/block-14-automated-tests-with-RTL/14.2/testes-react/src/logic/service.js
@@ -1,3 +1,9 @@
 const getRandomNumber = (min = 0, max = 100) => Math.floor(Math.random() * (max + 1)) + min;
 
-module.exports = { getRandomNumber };
+const upperCase = (string) => string.toUpperCase();
+
+const firstLetter = (string) => string[0];
+
+const concatStr = (str1, str2) => `${str1}${str2}`;
+
+module.exports = { getRandomNumber, upperCase, firstLetter, concatStr };
