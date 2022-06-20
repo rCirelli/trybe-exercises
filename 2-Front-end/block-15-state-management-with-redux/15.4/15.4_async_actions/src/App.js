d7ea829532a276147d6081eb14c08ec0694c86d4
Raphael Cirelli <raphael.cirelli@protonmail.com>
2022-06-20 16:02:33 -0300

    finished fixation exercises

--- 2-Front-end/block-15-state-management-with-redux/15.4/15.4_async_actions/src/App.js
+++ 2-Front-end/block-15-state-management-with-redux/15.4/15.4_async_actions/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchDog } from './store';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App({ isFetching, src, fetchDog }) {
+  return isFetching ? (
+    <p>Loading</p>
+  ) : (
+    <div className="App App-header">
+      <div
+        style={{ width: 500, display: 'flex', flexDirection: 'column-reverse' }}
+      >
+        <button style={{ display: 'block' }} onClick={fetchDog} type="button">
+          Novo Doguinho
+        </button>
+        <img style={{ width: '100%' }} src={src} alt="dog" />
+      </div>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  src: state.imagePath,
+  isFetching: state.isFetching,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchDog: () => dispatch(fetchDog()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
