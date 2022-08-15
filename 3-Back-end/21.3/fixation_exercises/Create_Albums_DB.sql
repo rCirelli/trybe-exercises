e966bbab45203f7fcee5113be82b92e3d56047ab
Raphael Cirelli <raphael.cirelli@protonmail.com>
2022-08-15 18:04:51 -0300

    update sql file

--- 3-Back-end/21.3/fixation_exercises/Create_Albums_DB.sql
+++ 3-Back-end/21.3/fixation_exercises/Create_Albums_DB.sql
@@ -18,13 +18,13 @@ CREATE TABLE album(
     title VARCHAR(255) NOT NULL,
     price DOUBLE NOT NULL,
     music_genre_id INT NOT NULL,
-    CONSTRAINT artist_id FOREIGN KEY (artist_id) REFERENCES artist (artist_id),
-    CONSTRAINT music_genre_id FOREIGN KEY (music_genre_id) REFERENCES music_genre (music_genre_id)
+    FOREIGN KEY (artist_id) REFERENCES artist (artist_id),
+    FOREIGN KEY (music_genre_id) REFERENCES music_genre (music_genre_id)
 );
 
 CREATE TABLE song(
 	song_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     album_id INT NOT NULL,
-    CONSTRAINT album_id FOREIGN KEY (album_id) REFERENCES album (album_id)
+    FOREIGN KEY (album_id) REFERENCES album (album_id)
 );
