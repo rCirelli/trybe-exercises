e17b5cb59e10f976dab4016578bf61e5e538acdf
Raphael Cirelli <raphael.cirelli@protonmail.com>
2022-08-22 14:44:51 -0300

    cleanup files
    only keep the files relevant to the day. files relative to other days are kept on their own branches

--- 1-fundamentals/block-8-JS-ES6-higher-order-functions/8.5/exercise2.js
+++
@@ -1,5 +0,0 @@
-// 2 - Crie uma função sum que dado um número variável de elementos retorna a soma desses elementos.
-
-const sum = (...numbers) => numbers.reduce((acc, curr) => acc + curr);
-
-console.log(sum(1,2,41,5,6,10,8,3,5));
--- 3-Back-end/20.4/exercise-10.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM BoxOffice
-WHERE movie_id IN(SELECT id FROM Movies WHERE length_minutes < 100);
-
-DELETE FROM Movies
-WHERE length_minutes < 100;
