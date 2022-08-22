143c176b679f9228b2564158adc65fffe9b4a8ce
Raphael Cirelli <raphael.cirelli@protonmail.com>
2022-08-22 16:39:32 -0300

    basic CRUD learning in express

--- 3-Back-end/22.2/soccer-team-manager/src/app.js
+++ 3-Back-end/22.2/soccer-team-manager/src/app.js
@@ -2,6 +2,64 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
+const teams = [
+  {
+    id: 1,
+    name: 'São Paulo Futebol Clube',
+    initials: 'SPF',
+  },
+  {
+    id: 2,
+    name: 'Clube Atlético Mineiro',
+    initials: 'CAM',
+  },
+];
+
 app.get('/', (req, res) => res.status(200).json({ message: 'Hello world!' }));
+
+app.get('/teams', (req, res) => res.status(200).json({ teams }));
+
+app.post('/teams', (req, res) => {
+  const newTeam = { ...req.body };
+  teams.push(newTeam);
+
+  res.status(201).json({ team: newTeam });
+});
+
+app.put('/teams/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, initials } = req.body;
+  let updatedTeam;
+
+  for (let i = 0; i < teams.length; i += 1) {
+    const team = teams[i];
+
+    if (team.id === Number(id)) {
+      team.name = name;
+      team.initials = initials;
+      updatedTeam = team;
+    }
+  }
+
+  res.status(200).json({ updatedTeam });
+});
+
+app.get('/teams/:id', (req, res) => {
+  const { id } = req.params;
+
+  const reqTeam = teams.find((team) => team.id === Number(id));
+
+  res.status(200).json({ reqTeam });
+});
+
+app.delete('/teams/:id', (req, res) => {
+  const { id } = req.params;
+  const index = teams.findIndex((team) => team.id === Number(id));
+  teams.splice(index, 1);
+
+  res.status(200).end();
+});
 
 module.exports = app;
