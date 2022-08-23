98ec2ad8413c0a0944df2d0d0e50ea34156c5427
Raphael Cirelli <raphael.cirelli@protonmail.com>
2022-08-23 15:10:46 -0300

    file celanup

--- 3-Back-end/20.4/exercise-3.sql
+++
@@ -1,3 +0,0 @@
-UPDATE Movies
-SET director = 'Andrew Staton'
-WHERE id = (SELECT id FROM (SELECT * FROM Movies WHERE title = 'Procurando Nemo') AS sub);
--- 3-Back-end/20.4/exercise-8.sql
+++
@@ -1,3 +0,0 @@
-UPDATE BoxOffice
-SET rating = 9.0
-WHERE domestic_sales > 400000000;
