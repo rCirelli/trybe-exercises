e1edb2ec0d34548360dda5b3038bf577a7061f83
Raphael Cirelli <raphael.cirelli@protonmail.com>
2022-08-24 19:07:35 -0300

    router middleware example

--- 3-Back-end/22.4/src/app.js
+++ 3-Back-end/22.4/src/app.js
@@ -1,71 +1,14 @@
 const express = require('express');
 require('express-async-errors');
 const morgan = require('morgan');
-const cors = require('cors');
-const validateTeam = require('./middlewares/validateTeam');
-const apiCredentials = require('./middlewares/apiCredentials');
+// require no nosso novo router
+const teamsRouter = require('./routes/teamsRouter');
 
 const app = express();
-app.use(cors());
 app.use(morgan('dev'));
+app.use(express.static('/images'));
 app.use(express.json());
-app.use(apiCredentials); 
-
-let nextId = 3;
-const teams = [
-  { id: 1, nome: 'São Paulo Futebol Clube', sigla: 'SPF' },
-  { id: 2, nome: 'Sociedade Esportiva Palmeiras', sigla: 'PAL' },
-];
-
-app.get('/teams', (req, res) => res.json(teams));
-
-const existingId = (req, res, next) => {
-  const id = Number(req.params.id);
-  const idExists = teams.some((t) => t.id === id);
-  if(idExists) {
-    next();
-  } else {
-    res.sendStatus(404);
-  }
-};
-
-app.get('/teams/:id', existingId, (req, res) => {
-  const id = Number(req.params.id);
-  const team = teams.find(t => t.id === id);
-  res.json(team);
-});
-
-// Arranja os middlewares para chamar validateTeam primeiro
-app.post('/teams', validateTeam, (req, res) => {
-  if (
-    // confere se a sigla proposta está inclusa nos times autorizados
-    !req.teams.teams.includes(req.body.sigla)
-    // confere se já não existe um time com essa sigla
-    && teams.every((t) => t.sigla !== req.body.sigla)
-  ) {
-    return res.sendStatus(401);
-  }
-  const team = { id: nextId, ...req.body };
-  teams.push(team);
-  nextId += 1;
-  res.status(201).json(team);
-});
-
-app.put('/teams/:id', existingId, validateTeam, (req, res) => {
-  const id = Number(req.params.id);
-  const team = teams.find(t => t.id === id);
-  const index = teams.indexOf(team);
-  const updated = { id, ...req.body };
-  teams.splice(index, 1, updated);
-  res.status(201).json(updated);
-});
-
-app.delete('/teams/:id', existingId, (req, res) => {
-  const id = Number(req.params.id);
-  const team = teams.find(t => t.id === id);
-  const index = teams.indexOf(team);
-  teams.splice(index, 1);
-  res.sendStatus(200);
-});
+// monta o router na rota /teams (1)
+app.use('/teams', teamsRouter);
 
 module.exports = app;
---
+++ 3-Back-end/22.4/src/routes/teamsRouter.js
@@ -0,0 +1,62 @@
+const express = require('express');
+// os requires são relativos ao arquivo, então o path muda ligeiramente
+const validateTeam = require('../middlewares/validateTeam');
+const apiCredentials = require('../middlewares/apiCredentials');
+
+
+// cria um router middleware
+const router = express.Router();
+
+let nextId = 3;
+const teams = [
+  { id: 1, nome: 'São Paulo Futebol Clube', sigla: 'SPF' },
+  { id: 2, nome: 'Sociedade Esportiva Palmeiras', sigla: 'PAL' },
+];
+
+// o path é relativo à rota em que o router foi montado (2)
+router.get('/', (req, res) => res.json(teams));
+
+// configurações globais afetam apenas este router (3)
+router.use(apiCredentials);
+
+router.get('/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const team = teams.find(t => t.id === id);
+  if (team) {
+    res.json(team);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+router.post('/', validateTeam, (req, res) => {
+  const team = { id: nextId, ...req.body };
+  teams.push(team);
+  nextId += 1;
+  res.status(201).json(team);
+});
+
+router.put('/:id', validateTeam, (req, res) => {
+  const id = Number(req.params.id);
+  const team = teams.find(t => t.id === id);
+  if (team) {
+    const index = teams.indexOf(team);
+    const updated = { id, ...req.body };
+    teams.splice(index, 1, updated);
+    res.status(201).json(updated);
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+router.delete('/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const team = teams.find(t => t.id === id);
+  if (team) {
+    const index = teams.indexOf(team);
+    teams.splice(index, 1);
+  }
+  res.sendStatus(200);
+});
+
+module.exports = router;
