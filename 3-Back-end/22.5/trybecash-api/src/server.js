e95198823f34293c9aeabb87176df6851f69da10
Raphael Cirelli <raphael.cirelli@protonmail.com>
2022-08-25 18:55:53 -0300

    fixation exercises complete
    all endpoints implemented

--- 3-Back-end/22.5/trybecash-api/src/server.js
+++ 3-Back-end/22.5/trybecash-api/src/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const app = require('./app');
 const connection = require('./db/connection');
 
@@ -7,9 +8,9 @@ app.listen(port, async () => {
   console.log(`API TrybeCash está sendo executada na porta ${port}`);
 
   // O código abaixo é para testarmos a comunicação com o MySQL
-  const [ result ] = await connection.execute('SELECT 1');
-  if (result) {
-    console.log('MySQL connection OK');
-  }
+  // const [ result ] = await connection.execute('SELECT 1');
+  // if (result) {
+  //   console.log('MySQL connection OK');
+  // }
 
 });
