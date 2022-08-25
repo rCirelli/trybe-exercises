a6917a18de3218052c313785cb9fdcc53be40ed1
Raphael Cirelli <raphael.cirelli@protonmail.com>
2022-08-25 15:05:04 -0300

    start fixation exercises

--- 3-Back-end/20.4/exercise-4.sql
+++
@@ -1,3 +0,0 @@
-UPDATE Movies
-SET title = 'Ratatouille', year = 2007
-WHERE id = (SELECT id FROM (SELECT * FROM Movies WHERE title = 'ratatui') AS sub);
--- 3-Back-end/21.2/fixation_exercises/inner-join-1.sql
+++
@@ -1,4 +0,0 @@
-SELECT a.actor_id, a.first_name, f.film_id
-FROM sakila.actor AS a
-INNER JOIN sakila.film_actor as f
-ON a.actor_id = f.actor_id;
