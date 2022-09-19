85ae60b24d3c3f232d839da9c4299d806cd3654c
Raphael Cirelli <raphael.cirelli@protonmail.com>
2022-09-19 14:59:54 -0300

    sequelize managed transaction example

--- 3-Back-end/6.2/sequelize-relations/src/services/employee.service.js
+++ 3-Back-end/6.2/sequelize-relations/src/services/employee.service.js
@@ -17,10 +17,29 @@ const getAll = async () => {
 };
 
 const insert = async ({ firstName, lastName, age, city, street, number }) => {
-  const employee = await Employee.create({ firstName, lastName, age });
+  const t = await sequelize.transaction();
+  try {
+    // Depois executamos as operações
+    const result = await sequelize.transaction(async (t) => {
+      const employee = await Employee.create({
+        firstName, lastName, age
+      }, { transaction: t });
 
-  await Address.create({ city, street, number, employeeId: employee.id });
-  return employee;
+      await Address.create({
+        city, street, number, employeeId: employee.id
+      }, { transaction: t });
+      return employee;
+    });
+    return result;
+    // Se chegou até aqui é porque as operações foram concluídas com sucesso,
+    // não sendo necessário finalizar a transação manualmente.
+    // `result` terá o resultado da transação, no caso um empregado e o endereço cadastrado
+  } catch (e) {
+    // Se entrou nesse bloco é porque alguma operação falhou.
+    // Nesse caso, o sequelize irá reverter as operações anteriores com a função rollback, não sendo necessário fazer manualmente
+    console.log(e);
+    throw e; // Jogamos o erro para a controller tratar
+  }
 };
 
 const getById = async (id) => {
